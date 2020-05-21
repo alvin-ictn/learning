a6c2f0643a216050d33256b20e5c07316b603cb9
Alvin Mantovani <alvin.ictn@gmail.com>
2020-05-22 01:33:05 +0700

    finished highlight logic for tagname

--- Progate/progate.js
+++ Progate/progate.js
@@ -1,14 +1,13 @@
 var myNodes = document.querySelectorAll(".codebook")
-elementlist = ["h1","h2","h3","h4"]
+elementlist = ["h1","h2","h3","h4","p"]
 var i = 0;
 const SetElementColor = (e) => {
   let newElement = ""
   e.innerHTML.split("<br>").forEach(d=>{
     let replace_element = d
-    if(d.includes != "comment"){
-      
+    if(d.includes("comment") != true){
       elementlist.forEach(c=>{
-        replace_element = replace_element.replace(c,`<a style='progate-element-color ${c}'>${c}</a>`)
+        replace_element = replace_element.split(c+"&gt;").join(`<a class='progate-element-color'>${c}</a>>`)
       })
     }else{
       replace_element
