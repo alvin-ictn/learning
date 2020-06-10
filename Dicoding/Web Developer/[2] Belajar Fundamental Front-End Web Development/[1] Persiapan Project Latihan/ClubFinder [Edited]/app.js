dcd87d9518acf81514733877837ebcada4a8efbb
Alvin Mantovani <alvin.ictn@gmail.com>
2020-06-10 17:13:02 +0700

    remake code

--- Dicoding/Web Developer/[2] Belajar Fundamental Front-End Web Development/[1] Persiapan Project Latihan/ClubFinder [Edited]/app.js
+++ Dicoding/Web Developer/[2] Belajar Fundamental Front-End Web Development/[1] Persiapan Project Latihan/ClubFinder [Edited]/app.js
@@ -1,2 +1,3 @@
+import "./src/script/component/app-bar.js";
 import main from "./src/script/view/main.js";
 document.addEventListener("DOMContentLoaded", main);
--- Dicoding/Web Developer/[2] Belajar Fundamental Front-End Web Development/[1] Persiapan Project Latihan/ClubFinder [Edited]/src/script/view/main.js
+++ Dicoding/Web Developer/[2] Belajar Fundamental Front-End Web Development/[1] Persiapan Project Latihan/ClubFinder [Edited]/src/script/view/main.js
@@ -1,48 +1,29 @@
+import '../component/club-list.js';
+import '../component/search-bar.js';
 import DataSource from '../data/data-source.js';
-
+ 
 const main = () => {
-    const searchElement = document.querySelector("#searchElement");
-    const buttonSearchElement = document.querySelector("#searchButtonElement");
-    const clubListElement = document.querySelector("#clubList");
-  
-  
-    const onButtonSearchClicked = async () => {
-        try{
-            const result = await DataSource.searchClub(searchElement.value);
-            renderResult(result);
-        } catch (message) {
-            fallbackResult(message)
-        }
-     };
-  
-  
-    const renderResult = results => {
-        clubListElement.innerHTML = "";
-        results.forEach(club => {
-            const {name, fanArt, description} = club;
-  
-            const clubElement = document.createElement("div");
-            clubElement.setAttribute("class", "club");
-  
-  
-            clubElement.innerHTML = `
-            <img class="fan-art-club" src="${fanArt}" alt=FanArt">
-            <div class="club-info">
-                <h2>${name}</h2>
-                <p>${description}</p>
-            </div>`
-            clubListElement.appendChild(clubElement);
-        })
-    };
-  
-  
-    const fallbackResult = message => {
-        clubListElement.innerHTML = "";
-        clubListElement.innerHTML += '<h2 class="placeholder">' + message + '</h2>'
-    };
-  
-  
-    buttonSearchElement.addEventListener("click", onButtonSearchClicked);
- };
-
- export default main;
+   const searchElement = document.querySelector("search-bar");
+   const clubListElement = document.querySelector("club-list");
+ 
+   const onButtonSearchClicked = async () => {
+       try {
+           const result = await DataSource.searchClub(searchElement.value);
+           renderResult(result);
+       } catch (message) {
+           fallbackResult(message)
+       }
+   };
+ 
+   const renderResult = results => {
+       clubListElement.clubs = results;
+   };
+ 
+   const fallbackResult = message => {
+       clubListElement.renderError(message);
+   };
+ 
+   searchElement.clickEvent = onButtonSearchClicked;
+};
+ 
+export default main;
